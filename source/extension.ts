46ac48b3947a92a0d8f5b762d0366e49af91af41
Federico Brigante <me@fregante.com>
2023-03-19 16:37:21 +0800

    Revert "Add status via CodeLens"
    This reverts commit d4c184d166f9183883ed5a17632abdc178c823c5.

--- source/extension.ts
+++ source/extension.ts
@@ -176,73 +176,6 @@ function createServer() {
 	}
 }
 
-/**
- * CodelensProvider
- */
-export class CodelensProvider implements vscode.CodeLensProvider {
-	private codeLenses: vscode.CodeLens[] = [];
-	private readonly regex: RegExp;
-	private readonly _onDidChangeCodeLenses: vscode.EventEmitter<void> =
-		new vscode.EventEmitter<void>();
-
-	// eslint-disable-next-line @typescript-eslint/member-ordering
-	public readonly onDidChangeCodeLenses: vscode.Event<void> =
-		this._onDidChangeCodeLenses.event;
-
-	constructor() {
-		this.regex = /(.+)/g;
-
-		vscode.workspace.onDidChangeConfiguration((_) => {
-			this._onDidChangeCodeLenses.fire();
-		});
-	}
-
-	public provideCodeLenses(
-		document: vscode.TextDocument,
-		token: vscode.CancellationToken,
-	): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
-		if (
-			vscode.workspace
-				.getConfiguration('codelens-sample')
-				.get('enableCodeLens', true)
-		) {
-			this.codeLenses = [];
-			const regex = new RegExp(this.regex);
-			const text = document.getText();
-			let matches;
-			while ((matches = regex.exec(text)) !== null) {
-				const line = document.lineAt(document.positionAt(matches.index).line);
-				const indexOf = line.text.indexOf(matches[0]);
-				const position = new vscode.Position(line.lineNumber, indexOf);
-				const range = document.getWordRangeAtPosition(
-					position,
-					new RegExp(this.regex),
-				);
-				if (range) {
-					this.codeLenses.push(new vscode.CodeLens(range));
-				}
-			}
-
-			return this.codeLenses;
-		}
-
-		return [];
-	}
-
-	public resolveCodeLens(
-		codeLens: vscode.CodeLens,
-		token: vscode.CancellationToken,
-	) {
-		codeLens.command = {
-			title: 'Codelens provided by sample extension',
-			tooltip: 'Tooltip provided by sample extension',
-			command: 'codelens-sample.codelensAction',
-			arguments: ['Argument 1', false],
-		};
-		return codeLens;
-	}
-}
-
 export function activate(_context: vscode.ExtensionContext) {
 	context = _context;
 	createServer();
@@ -258,8 +191,4 @@ export function activate(_context: vscode.ExtensionContext) {
 		null,
 		context.subscriptions,
 	);
-
-	const codelensProvider = new CodelensProvider();
-
-	vscode.languages.registerCodeLensProvider('*', codelensProvider);
 }
